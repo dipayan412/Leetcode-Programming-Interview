c83f549a81a9e6047ec3424b8229b77882901317
dipayan <dipayan412@gmail.com>
2017-08-19 20:34:45 -0700

    Validate Binary Search Tree

--- Leetcode98.java
+++ Leetcode98.java
@@ -1,29 +1,32 @@
 class TreeNode 
 {
-	int val;
+    int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+    TreeNode(int x) 
+    { 
+    	val = x; 
+    }
  }
 
 public class LeetCode98 
 {
-	public boolean isValidBST(TreeNode root) 
+    public boolean isValidBST(TreeNode root) 
     {
         return validateBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
     public boolean validateBST(TreeNode root, long minVal, long maxVal)
+    {
+	if(root == null)
 	{
-		if(root == null)
-		{
-			return true;
-		}
-		if(root.val >= maxVal || root.val <= minVal)
-		{
-			return false;
-		}
-		
-		return validateBST(root.left, minVal, root.val) && validateBST(root.right, root.val, maxVal);
+	    return true;
 	}
+	if(root.val >= maxVal || root.val <= minVal)
+	{
+	    return false;
+	}
+		
+	return validateBST(root.left, minVal, root.val) && validateBST(root.right, root.val, maxVal);
+    }
 }
